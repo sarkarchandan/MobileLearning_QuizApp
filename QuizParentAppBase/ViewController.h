0739fb61cad5aa9618996226da84acc26ec8c973
chandan <elitegreen@gmail.com>
2018-06-23 21:34:55 +0200

    Initial Commit

---
+++ QuizParentAppBase/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuizParentAppBase
+//
+//  Created by Chandan Sarkar on 23.06.18.
+//  Copyright © 2018 Chandan Sarkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
