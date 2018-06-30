cba24c60fc4f8fd8e553a85661a0972e5863a221
chandan <elitegreen@gmail.com>
2018-06-30 21:28:34 +0200

    Added code to update the UI according to the quiz progress

--- Quiz_ObjC/Controller/MainViewController.m
+++ Quiz_ObjC/Controller/MainViewController.m
@@ -10,6 +10,7 @@
 @property(nonatomic)NSInteger questionNumber;
 @property(nonatomic)QuestionBank* questionBank;
 @property(nonatomic)BOOL pickedAnswer;
+@property(nonatomic)NSInteger score;
 @end
 
 @implementation MainViewController
@@ -33,6 +34,7 @@
     [super viewDidLoad];
     _questionBank = [[QuestionBank alloc] init];
     [self setPickedAnswer:NO];
+    [self prepareUI];
     [self setQuestionNumber:0];
     [self loadNextQuestion];
 }
@@ -42,35 +44,57 @@
     if ([self questionNumber] < [[[self questionBank] questions] count])
     {
         [[self questionLabel] setText: [[[[self questionBank] questions]objectAtIndex:_questionNumber]questionText]];
+        [self updateProgress];
     }else
     {
-        [self createEndOfQuizAlert];
+        [self restartQuiz];
     }
 }
 
--(void)createEndOfQuizAlert
+-(void)restartQuiz
 {
     __weak typeof(self) weakSelf = self;
     UIAlertController* controller = [UIAlertController alertControllerWithTitle:@"End of Quiz" message:@"The Quiz has been ended, do you want to start over" preferredStyle:UIAlertControllerStyleAlert];
     UIAlertAction* restartAction = [UIAlertAction actionWithTitle:@"Restart" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action)
                                     {
                                         [weakSelf setQuestionNumber:0];
+                                        [weakSelf prepareUI];
                                         [weakSelf loadNextQuestion];
                                     }];
     [controller addAction:restartAction];
     [self presentViewController:controller animated:YES completion:nil];
 }
 
+-(void) prepareUI
+{
+    _score = 0;
+    [[self scoreLabel] setText: [NSString stringWithFormat:@"%@: %ld",@"Score",[self score]]];
+    [[self progressLabel] setText: [NSString stringWithFormat:@"%ld / %ld",[self questionNumber]+1,[[[self questionBank] questions]count]]];
+}
+
+-(void)updateScore
+{
+    _score += 1;
+    [[self scoreLabel] setText: [NSString stringWithFormat:@"%@: %ld",@"Score",[self score]]];
+}
+
+-(void)updateProgress
+{
+    [[self progressLabel] setText: [NSString stringWithFormat:@"%ld / %ld",[self questionNumber]+1,[[[self questionBank] questions]count]]];
+    CGRect progressViewFrame = [[self progressView] frame];
+    progressViewFrame.size.width = ([[self view] frame].size.width / 13) * (CGFloat)([self questionNumber] + 1);
+    [[self progressView] setFrame:progressViewFrame];
+}
+
 -(void)checkAnswer
 {
     if([self pickedAnswer] == [[[[self questionBank] questions]objectAtIndex:[self questionNumber]]answer])
     {
-        NSLog(@"Correct Answer");
+        [self updateScore];
     }else
     {
         NSLog(@"Wrong Answer");
     }
-    
 }
 
 @end
--- Quiz_Swift/Controller/MainViewController.swift
+++ Quiz_Swift/Controller/MainViewController.swift
@@ -29,21 +29,24 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadNextQuestion()
+        self.prepareUI()
+        self.loadNextQuestion()
     }
     
     private func loadNextQuestion(){
         guard questionNumber < questionBank.questions.count else {
-            createEndOfQuizAlert()
+            restartQuiz()
             return
         }
         questionLabel.text = questionBank.questions[questionNumber].questionText
+        self.updateProgress()
     }
     
-    private func createEndOfQuizAlert(){
+    private func restartQuiz(){
         let alert = UIAlertController(title: "End of Quiz", message: "The Quiz has ended, do you want to start over", preferredStyle: .alert)
         let action = UIAlertAction(title: "Restart", style: .default) {[unowned self] _ in
             self.questionNumber = 0
+            self.prepareUI()
             self.loadNextQuestion()
         }
         alert.addAction(action)
@@ -53,14 +56,31 @@ class MainViewController: UIViewController {
     private func checkAnswer() {
         guard questionNumber < questionBank.questions.count else { return }
         if pickedAnswer == questionBank.questions[questionNumber].answer {
-            print("Correct Answer")
+            self.score += 1
+            self.updateScore()
         }else {
             print("wrong Answer")
         }
     }
     
+    private func prepareUI(){
+        self.score = 0
+        self.scoreLabel.text = "Score: \(score)"
+        self.progressLabel.text = "\(self.questionNumber + 1) / \(self.questionBank.questions.count)"
+    }
+    
+    private func updateScore(){
+        self.scoreLabel.text = "Score: \(score)"
+    }
+    
+    private func updateProgress(){
+        self.progressLabel.text = "\(self.questionNumber + 1) / \(self.questionBank.questions.count)"
+        self.progressView.frame.size.width = (self.view.frame.size.width / 13) * CGFloat(self.questionNumber + 1)
+    }
+    
     private let questionBank = QuestionBank()
     private var pickedAnswer = false
     private var questionNumber = 0
+    private var score = 0
 }
 
